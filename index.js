befdf9be1674f35f4d6ae86f176865886c9dc6be
Lars-Erik Roald <lars.roald@gmail.com>
2012-12-02 16:19:51 +0100

    correct index.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-(function avoid_caching_when_module() {
-	delete require.cache[module.id];
-	delete require.cache[require.resolve('./when/when')];
-})();
-
 module.exports = {
-	when: require('./when/when'),
 	mock: require('./mock'),
 	expectRequire: require('./expectRequire'),
 	requireMock: require('./requireMock')
